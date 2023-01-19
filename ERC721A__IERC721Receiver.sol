2078a57b3a7660e09ec9e4096baf465743933f41
nikaluka <47041738+nikaluka@users.noreply.github.com>
2023-01-19 22:52:09 +0100

    Add files via upload

---
+++ ERC721A__IERC721Receiver.sol
@@ -0,0 +1,19 @@
+// SPDX-License-Identifier: MIT
+// ERC721A Contracts v4.2.3
+// Creator: Chiru Labs
+
+pragma solidity ^0.8.4;
+
+import './IERC721A.sol';
+
+/**
+ * @dev Interface of ERC721 token receiver.
+ */
+interface ERC721A__IERC721Receiver {
+    function onERC721Received(
+        address operator,
+        address from,
+        uint256 tokenId,
+        bytes calldata data
+    ) external returns (bytes4);
+}
